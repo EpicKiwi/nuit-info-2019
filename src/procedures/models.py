37807f5742b0ebdd6e6e8a7a5185294d739ad85d
4rtefakt <koreshingan@gmail.com>
2019-12-05 21:43:18 +0100

    Modèles Article et User ajoutés

--- src/procedures/models.py
+++ src/procedures/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=70)
+    surname = models.CharField(max_length=70)
+    location = models.CharField(max_length=70)
+
+
+    def __str__(self):
+        return self.name + ' ' + self.surname
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=70)
+    tag = models.CharField(max_length=70)
+    location = models.CharField(max_length=70)
+    text = models.CharField(max_length=70)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+
