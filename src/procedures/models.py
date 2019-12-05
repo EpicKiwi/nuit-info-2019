3f97a4fb427105f1035ba5eacb3ab257cd9e30d6
4rtefakt <koreshingan@gmail.com>
2019-12-05 22:28:23 +0100

    dejalebazar

--- src/procedures/models.py
+++ src/procedures/models.py
@@ -7,7 +7,6 @@ class User(models.Model):
     surname = models.CharField(max_length=70)
     location = models.CharField(max_length=70)
 
-
     def __str__(self):
         return self.name + ' ' + self.surname
 
@@ -16,10 +15,34 @@ class Article(models.Model):
     title = models.CharField(max_length=70)
     tag = models.CharField(max_length=70)
     location = models.CharField(max_length=70)
-    text = models.CharField(max_length=70)
+    text = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    date = models.DateField()
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    score = models.IntegerField()
+    date = models.DateField()
+    location = models.CharField(max_length=70)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    step = models.ForeignKey(Step, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    score = models.IntegerField()
+    date = models.DateField()
+    location = models.CharField(max_length=70)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    step = models.ForeignKey(Step, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
 
