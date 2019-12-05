ec4642ca9c0e4c72f18c7941a960e0616a7d87a7
AntoineCaillet <antoine.caillet@viacesi.fr>
2019-12-06 00:14:42 +0100

    somes changes on models.py

--- src/procedures/models.py
+++ src/procedures/models.py
@@ -1,50 +1,59 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=70)
+    name = models.CharField(max_length=70, unique=True, help_text='--> nom du tag')
 
     def __str__(self):
-        return self.title
+        return self.name
 
 
 class Badge(models.Model):
     picture = models.ImageField()
-    name = models.CharField(max_length=70)
+    name = models.CharField(max_length=70, unique=True, help_text='--> nom du badge')
 
     def __str__(self):
-        return self.title
+        return self.name
+
+
+class File(models.Model):
+    text = models.TextField(help_text='Description du fichier')
+    file = models.FileField()
+
+    def __str__(self):
+        return self.file.attname
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=128)
+    surname = models.CharField(max_length=128)
+    location = models.CharField(max_length=128)
+    creationDate = models.DateTimeField()
+    badge = models.ManyToManyField(Badge, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.surname
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=70)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
     location = models.CharField(max_length=70)
     text = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    updateDate = models.DateField()
-    helpers = models.ManyToManyField(User, on_delete=models.CASCADE)
-    duration = models.durationField()
+    updateDate = models.DateTimeField()
+    duration = models.DurationField()
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    helpers = models.ManyToManyField(Student, on_delete=models.CASCADE)
+    author = models.ForeignKey(Student, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
 
-class User(models.Model):
-    name = models.CharField(max_length=70)
-    surname = models.CharField(max_length=70)
-    location = models.CharField(max_length=70)
-    badge = models.ManyToManyField(Badge, on_delete=models.CASCADE)
-    creationDate = models.DateField()
-
-    def __str__(self):
-        return self.name + ' ' + self.surname
-
-
 class Step(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=200)
     order = models.IntegerField()
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
 
@@ -53,15 +62,17 @@ class Step(models.Model):
 
 
 class ContentStep(models.Model):
+    ONLINE = 'ONL'
+    IN_REAL_LIFE = 'IRL'
     TYPE_CHOICES = [
-        ( 'ONL', 'En ligne' ),
-        ( 'IRL', 'Physique' ),
+        (ONLINE, 'En ligne'),
+        (IN_REAL_LIFE, 'Physique'),
     ]
     type = models.CharField(max_length=3, choices=TYPE_CHOICES, default='ONL')
     text = models.TextField()
-    duration = models.durationField()
+    duration = models.DurationField()
     step = models.ForeignKey(Step, on_delete=models.CASCADE)
-    file = ForeignKey(File, on_delete=models.CASCADE)
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.text
@@ -69,11 +80,11 @@ class ContentStep(models.Model):
 
 class Comment(models.Model):
     text = models.TextField()
-    tag = models.CharField(max_length=20)
     like = models.IntegerField()
     date = models.DateField()
-    file = models.ForeignKey(File, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.ManyToOneRel(File, on_delete=models.CASCADE)
+    user = models.ForeignKey(Student, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag, on_delete=models.CASCADE)
     contentStep = models.ForeignKey(ContentStep, on_delete=models.CASCADE)
 
     def __str__(self):
