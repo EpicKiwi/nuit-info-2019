ac734b4184d1e688efe08b54259986b03601e077
Tanguy <tanguy.blochet@viacesi.fr>
2019-12-06 01:40:07 +0100

    merge model + template

--- src/procedures/views.py
+++ src/procedures/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 
 def list_procedures(request):
     return render(request, 'procedures/list_procedures.html')
+
+
+def procedures(request, article_slug):
+    return render(request, "procedures.html", {'article': article_slug})
+
