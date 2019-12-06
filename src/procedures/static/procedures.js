365b839d77006443c83af04230c30029bce14cb9
Tanguy <tanguy.blochet@viacesi.fr>
2019-12-06 07:22:45 +0100

    js

---
+++ src/procedures/static/procedures.js
@@ -0,0 +1,24 @@
+function check(id) {
+  // Get the checkbox
+  var checkBox = document.getElementById("checkbox-"+id);
+  var tip = document.getElementById("tips-"+id);
+  var form = document.getElementById("form-"+id);
+  if (checkBox.checked){
+      //change tip to tip-hidden
+      tip.classList.add("tip-hidden");
+      tip.classList.remove("tip");
+
+      //comment-form en comment-form-hidden
+
+      console.log(form);
+      form.classList.remove("comment-form-hidden");
+      form.classList.add("comment-form");
+  }
+  else {
+      tip.classList.add("tip");
+      tip.classList.remove("tip-hidden");
+
+      form.classList.add("comment-form-hidden");
+      form.classList.remove("comment-form");
+  }
+}
