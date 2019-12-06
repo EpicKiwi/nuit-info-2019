cec28f6d639d3338716edfc2f795ca09cab9506d
EtienneDuv <etienne.duverney@viacesi.fr>
2019-12-06 02:15:44 +0100

    All procedures view

--- src/procedures/views.py
+++ src/procedures/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from procedures.models import Article
 
 
 def list_procedures(request):
-    return render(request, 'procedures/list_procedures.html')
+    allProcedures = Article.objects.all()
+    return render(request, 'procedures/list_procedures.html', {"procedures": allProcedures})
